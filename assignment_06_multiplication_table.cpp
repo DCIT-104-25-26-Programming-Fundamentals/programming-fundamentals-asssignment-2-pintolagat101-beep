df3417201418f91c05248107c938cbc85afecae0
Pinto Clifford Tetteh <pintolagat101@gmail.com>
2026-08-01 13:06:42 +0000

    multiplication table completed

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -57,3 +57,52 @@
 #include <iostream>
 using namespace std;
 
+void printFibonacci(int n) {
+    int a = 0, b = 1;
+    cout << "Fibonacci sequence: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << endl;
+}
+
+bool isFibonacci(int num) {
+    if (num < 0) return false;
+    int a = 0, b = 1;
+    while (a <= num) {
+        if (a == num) return true;
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    return false;
+}
+
+int main() {
+    int n;
+    cout << "How many terms? ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return 0;
+    }
+
+    printFibonacci(n);
+
+    int num;
+    cout << "\nEnter a number to check: ";
+    cin >> num;
+
+    if (isFibonacci(num)) {
+        cout << num << " is a Fibonacci number." << endl;
+    } else {
+        cout << num << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
+
